0381ac339624734adef454e70d8183467ed99beb
sfkashan <sfkashan@gmail.com>
2014-09-21 15:40:11 -0400

    Revert "PrgAssgn2"
    This reverts commit 831829ea79c25a74c186ab764ee803fb1bcb8db0.

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,15 @@
-## A pair of functions that cache the inverse of a matrix
+## Put comments here that give an overall description of what your
+## functions do
 
-
-## This function creates a special "matrix" object that can cache its inverse
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        i <- NULL
-        set <- function(m){
-                x <<- m
-                i <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(inverse) i <<- inverse
-        getinverse <- function () i
-        list (set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+
 }
 
 
-## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated, 
-## then cacheSolve should retrieve the inverse from the cache.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        i  <- x$getinverse()
-        if (!is.null(i)){
-                message("Getting cached data")
-                return(i)
-        }
-        data  <- x$get()
-        i  <- solve(data, ...)
-        x$setinverse(i)
-        i
+        ## Return a matrix that is the inverse of 'x'
 }
